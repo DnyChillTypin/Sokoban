c0cd1c5f2d30de222a1cf5b1d08fee3bce4461c5
Nguyen Manh Tien <tien231230923@lms.utc.edu.vn>
2026-04-06 23:42:26 +0700

    Final version

--- selectLevels.py
+++ selectLevels.py
@@ -20,7 +20,7 @@ class LevelSelection:
         self.dark_overlay.fill((0, 0, 0))
         self.dark_overlay.set_alpha(150)
 
-        self.current_level = 1
+        self.current_level = 0
         self.selected_level = None
 
         self.level_cache = {}
@@ -72,7 +72,7 @@ class LevelSelection:
     def _load_level_preview(self):
         if self.current_level not in self.level_cache:
             if not os.path.exists(f"levels/{self.current_level}.txt"):
-                self.current_level = 1
+                self.current_level = 0
 
             if self.current_level not in self.level_cache:
                 self.level_cache[self.current_level] = Level(self.current_level)
@@ -80,12 +80,21 @@ class LevelSelection:
             self.level_cache[self.current_level] = Level(self.current_level)
 
         level = self.level_cache[self.current_level]
+        self.box_count = len(level.boxes)
 
         map_width = level.columns * scaled_tile
         map_height = level.rows * scaled_tile
 
         surface = pygame.Surface((map_width, map_height), pygame.SRCALPHA)
         level.draw(surface)
+
+        player_img = pygame.image.load(textures['player']).convert_alpha()
+        player_img = pygame.transform.scale(player_img, (scaled_tile, scaled_tile))
+
+        player_x = level.player_start_x * scaled_tile
+        player_y = level.player_start_y * scaled_tile
+
+        surface.blit(player_img, (player_x, player_y))
 
         DESIRED_TILE = 60
 
@@ -98,8 +107,13 @@ class LevelSelection:
 
         self.preview_img = pygame.transform.smoothscale(surface, (preview_w, preview_h))
 
-        self.preview_rect = self.preview_img.get_rect(
-            center=(self.width // 2, self.height // 2+30)
+        TOP_OFFSET = 230
+
+        self.preview_rect = pygame.Rect(
+            (self.width - preview_w) // 2,
+            TOP_OFFSET,
+            preview_w,
+            preview_h
         )
 
     def handle_events(self, event):
@@ -114,7 +128,7 @@ class LevelSelection:
         if event.type == pygame_gui.UI_BUTTON_PRESSED:
 
             if event.ui_element == self.left_btn:
-                self.current_level = max(1, self.current_level - 1)
+                self.current_level = max(0, self.current_level - 1)
                 self._load_level_preview()
 
             elif event.ui_element == self.right_btn:
@@ -135,19 +149,36 @@ class LevelSelection:
         font = pygame.font.Font(font_path, 80)
 
         txt = font.render(
-            f"LEVEL {self.current_level}", True, (0, 255, 127)
+            f"LEVEL {self.current_level + 1}", True, (0, 255, 127)
         )
 
         shadow = font.render(
-            f"LEVEL {self.current_level}", True, (0, 50, 0)
+            f"LEVEL {self.current_level + 1}", True, (0, 50, 0)
         )
 
+        TITLE_Y = 80
         level_x = self.preview_rect.centerx - txt.get_width() // 2
-        level_y = self.preview_rect.top - 80
+        level_y = TITLE_Y
 
         self.screen.blit(shadow, (level_x + 5, level_y + 5))
 
         self.screen.blit(txt, (level_x, level_y))
+
+        box_font = pygame.font.Font(font_path, 50)
+
+        box_text = box_font.render(
+            f"Boxes: {self.box_count}", True, (0, 255, 127)
+        )
+        shadow = box_font.render(
+            f"Boxes: {self.box_count}", True, (0, 50, 0)
+        )
+
+        BOX_Y = 800
+        box_x = self.preview_rect.centerx - box_text.get_width() // 2
+        box_y = BOX_Y
+
+        self.screen.blit(shadow, (box_x + 2, box_y + 2))
+        self.screen.blit(box_text, (box_x, box_y))
 
         self.manager.update(0.016)
         self.manager.draw_ui(self.screen)
